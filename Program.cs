3575b603067fc00ae68b247e0649521235793a6b
lexvonghost <lex_von_tost@mail.ru>
2022-08-14 13:22:26 +0300

    build: добавил условия задачи

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите число a:");
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите число a:");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число b:");
 int b = Convert.ToInt32(Console.ReadLine());
